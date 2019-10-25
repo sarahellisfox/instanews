191df822d889c68d70d0a8db4b085d3f484aa96d
sarahellisfox <sarah.ellis.fox@gmail.com>
2019-10-24 20:15:14 -0700

    API connected -- gets image/article

--- js/script.js
+++ js/script.js
@@ -1,33 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-    $('#selector').on('click', function () {
-        $.ajax({
+    $('#optionSelector').on('change', function () {
+        const selection = $('#optionSelector option:selected').val();
+            $.ajax({
             method: 'GET',
-            url: 'https://api.nytimes.com/svc/topstories/v2/science.json?api-key=fJ9SlU4rVsUv2yCqN5v9e61T29VDHSKa'
-        })
-            .done(function (data) {
-                console.log(data);
+            url: `https://api.nytimes.com/svc/topstories/v2/${selection}.json?api-key=fJ9SlU4rVsUv2yCqN5v9e61T29VDHSKa`,
+        }).done(function (data) {
+            $('.results').empty(); 
+            $('.results').append('<img src="' + data.results[0].multimedia[4].url + '">');
+            $('.results').append('<h1>' + data.results[0].abstract + '</h1>');
+        
             })
     });
 });
 
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     $('button').on('click', function () {
-//         $.ajax({
-//             method: 'GET',
-//             url: 'https://api.openweathermap.org/data/2.5/weather?q=Vancouver,ca&appid=4a48e1e1428fd83889074671fbf259d9'
-//         }).done(function (data) {
-//             const currentWeather = data.weather[0].main;
-//             const weatherDesc = data.weather[0].description;
-//             const weatherIcon = data.weather[0].icon;
-
-//             $('.results').empty();
-//             $('.results').append('<img src="http://openweathermap.org/img/wn/' + weatherIcon + '.png">');
-//             $('.results').append('<p>' + currentWeather + '</p>');
-//             $('.results').append('<p>' + weatherDesc + '</p>');
-//             $('.weatherText').remove();
-//             ;
-//         })
-//     })
-// });
