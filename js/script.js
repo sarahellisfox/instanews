0fdbdb267f7623e2740b6e816dec36f84d5a06d5
sarahellisfox <sarah.ellis.fox@gmail.com>
2019-10-31 14:25:47 -0700

    Changed generated articles from img to background images

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
             method: 'GET',
             url: `https://api.nytimes.com/svc/topstories/v2/${selection}.json?api-key=fJ9SlU4rVsUv2yCqN5v9e61T29VDHSKa`,
         }).done(function (data) {
-            $('.generated-articles').empty();
+            $('.generated-articles-container').empty();
             const filteredResults = data.results.filter(function (result) {
                 return result.multimedia[4] !== undefined;
             });
@@ -13,10 +13,11 @@ document.addEventListener('DOMContentLoaded', function () {
                 $('.generated-articles').append('<p>Sorry, nothing found! Please try again.</p>');
             } else {
                 filteredResults.slice(0, 12).forEach(function (item) {
-                    $('.generated-articles').append('<img src="' + item.multimedia[4].url + '">');
-                    $('.generated-articles').append('<h1>' + item.abstract + '</h1>');
+                    $('.generated-articles-container').append('<article class="generated-articles" style="background-image: url(' + item.multimedia[4].url + ');">' + '<p class="article-caption">' + item.abstract + '</p></article>');
                 });
             }
         });
     });
 });
+
+
