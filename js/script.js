530fb78014397afddb11909d822a38912ce6d9b3
sarahellisfox <sarah.ellis.fox@gmail.com>
2019-10-30 19:57:41 -0700

    Working API - filters 12 articles

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     $('#optionSelector').on('change', function () {
         const selection = $('#optionSelector option:selected').val();
-            $.ajax({
+        $.ajax({
             method: 'GET',
             url: `https://api.nytimes.com/svc/topstories/v2/${selection}.json?api-key=fJ9SlU4rVsUv2yCqN5v9e61T29VDHSKa`,
         }).done(function (data) {
-            $('.results').empty(); 
-            $('.results').append('<img src="' + data.results[0].multimedia[4].url + '">');
-            $('.results').append('<h1>' + data.results[0].abstract + '</h1>');
-        
-            })
+            $('.generated-articles').empty();
+            const filteredResults = data.results.filter(function (result) {
+                return result.multimedia[4] !== undefined;
+            });
+            if (filteredResults.length === 0) {
+                $('.generated-articles').append('<p>Sorry, nothing found! Please try again.</p>');
+            } else {
+                filteredResults.slice(0, 12).forEach(function (item) {
+                    $('.generated-articles').append('<img src="' + item.multimedia[4].url + '">');
+                    $('.generated-articles').append('<h1>' + item.abstract + '</h1>');
+                });
+            }
+        });
     });
 });
-
