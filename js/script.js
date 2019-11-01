b3612a6b64e08dc35277005b97a405d6bf6e46e1
sarahellisfox <sarah.ellis.fox@gmail.com>
2019-11-01 09:22:34 -0700

    fixed error message

--- js/script.js
+++ js/script.js
@@ -3,19 +3,21 @@ document.addEventListener('DOMContentLoaded', function () {
         const selection = $('#optionSelector option:selected').val();
         $.ajax({
             method: 'GET',
-            url: `https://api.nytimes.com/svc/topstories/v2/${selection}.json?api-key=fJ9SlU4rVsUv2yCqN5v9e61T29VDHSKa`,
+            url: `http://api.nytimes.com/svc/topstories/v2/${selection}.json?api-key=fJ9SlU4rVsUv2yCqN5v9e61T29VDHSKa`,
         }).done(function (data) {
             $('.generated-articles-container').empty();
             const filteredResults = data.results.filter(function (result) {
                 return result.multimedia[4] !== undefined;
             });
             if (filteredResults.length === 0) {
-                $('.generated-articles').append('<p>Sorry, nothing found! Please try again.</p>');
+                $('.generated-articles-container').append('<p>Sorry, nothing found! Please try again.</p>');
             } else {
                 filteredResults.slice(0, 12).forEach(function (item) {
                     $('.generated-articles-container').append('<article class="generated-articles" style="background-image: url(' + item.multimedia[4].url + ');">' + '<p class="article-caption">' + item.abstract + '</p></article>');
                 });
             }
+        }).fail(function() {
+            $('.generated-articles-container').append('<p>Sorry, nothing found! Please try again.</p>');
         });
     });
 });
