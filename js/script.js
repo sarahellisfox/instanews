b1f604c2c044e9ce7b65b1b0e838cd8e0b113650
sarahellisfox <sarah.ellis.fox@gmail.com>
2019-11-01 10:09:28 -0700

    added loading bar - fully functioning

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     $('#optionSelector').on('change', function () {
+        $('#loading-icon').show();
+    
         const selection = $('#optionSelector option:selected').val();
+
         $.ajax({
             method: 'GET',
             url: `http://api.nytimes.com/svc/topstories/v2/${selection}.json?api-key=fJ9SlU4rVsUv2yCqN5v9e61T29VDHSKa`,
@@ -16,8 +19,10 @@ document.addEventListener('DOMContentLoaded', function () {
                     $('.generated-articles-container').append('<article class="generated-articles" style="background-image: url(' + item.multimedia[4].url + ');">' + '<p class="article-caption">' + item.abstract + '</p></article>');
                 });
             }
-        }).fail(function() {
+        }).fail(function () {
             $('.generated-articles-container').append('<p>Sorry, nothing found! Please try again.</p>');
+        }).always(function() {
+            $('#loading-icon').hide();
         });
     });
 });
