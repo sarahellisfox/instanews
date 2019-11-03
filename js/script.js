00e73dc728b677a72813db211d67923ad15d0ce8
sarahellisfox <sarah.ellis.fox@gmail.com>
2019-11-02 21:41:33 -0700

    fixed the font for the dropdown menu!

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-    // Class Variables 
     const optionSelector = document.getElementById('optionSelector');
 
     // Initiates the Selectric plugin to change the style of the dropdown selector 
@@ -40,25 +38,25 @@ document.addEventListener('DOMContentLoaded', function () {
                 filteredResults.slice(0, 12).forEach(function (item) {
 
                     //Appends the article container with a link, image and article caption  
-                    $('.generated-articles-container').append('<a href="' + item.url + '">' + '<article class="generated-articles" style="background-image: url(' + item.multimedia[4].url + ');">' + '<p class="article-caption">' + item.abstract + '</p></article></a>');
+                    $('.generated-articles-container').append('<a href="' + item.url + '">' + '<div class="article-text-popup"><article class="generated-articles" style="background-image: url(' + item.multimedia[4].url + ');">' + '<p class="article-caption">' + item.abstract + '</p></article></div></a>');
 
                     // Changes header, nav and logo from large to small when results are returned 
                     $("img").removeClass("large-logo").addClass("small-logo").wrap('<a href="https://sarahellisfox.github.io/instanews/"></a>');
-                $("main").removeClass("main-container-large").addClass("main-container-small");
-                $("header").removeClass("header-container-large").addClass("header-container-small");
-                $("nav").removeClass("navigation-container-large").addClass("navigation-container-small");
+                    $("main").removeClass("main-container-large").addClass("main-container-small");
+                    $("header").removeClass("header-container-large").addClass("header-container-small");
+                    $("nav").removeClass("navigation-container-large").addClass("navigation-container-small");
 
-            });
-    }
+                });
+            }
 
             //If the above request fails entirely this error message is displayed. Empty is used to keep the error messages from spawning each time it is returned
         }).fail(function () {
-        $('.generated-articles-container').empty();
-        $('.generated-articles-container').append('<p>Sorry, nothing found! Please try again.</p>');
+            $('.generated-articles-container').empty();
+            $('.generated-articles-container').append('<p>Sorry, nothing found! Please try again.</p>');
 
-        // Hides the loading icon after content is loaded
-    }).always(function () {
-        $('#loading-icon').hide();
-    });
+            // Hides the loading icon after content is loaded
+        }).always(function () {
+            $('#loading-icon').hide();
+        });
     });
 });
