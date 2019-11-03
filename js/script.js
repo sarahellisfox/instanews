a383a721be5635dab03509b8d43e59499654955f
sarahellisfox <sarah.ellis.fox@gmail.com>
2019-11-02 21:10:38 -0700

    flex goal 1 - added jquery plugin for menu customization

--- js/script.js
+++ js/script.js
@@ -1,35 +1,64 @@
 document.addEventListener('DOMContentLoaded', function () {
-    $('#optionSelector').on('change', function () {
+
+    // Class Variables 
+    const optionSelector = document.getElementById('optionSelector');
+
+    // Initiates the Selectric plugin to change the style of the dropdown selector 
+    $(function () {
+        $(optionSelector).selectric();
+    });
+
+    // Triggers event on when user selects any of the options on dropdown selector 
+    $(optionSelector).on('change', function () {
+
+        // Identifies what has been selected from the dropdown selector 
+        const selection = $(optionSelector).val();
+
+        //Reveals loading icon, which is hidden by default  
         $('#loading-icon').show();
 
-        const selection = $('#optionSelector option:selected').val();
-
+        // Uses ajax to GET data from the NYT API 
         $.ajax({
             method: 'GET',
-            url: `http://api.nytimes.com/svc/topstories/v2/${selection}.json?api-key=fJ9SlU4rVsUv2yCqN5v9e61T29VDHSKa`,
+            url: `https://api.nytimes.com/svc/topstories/v2/${selection}.json?api-key=fJ9SlU4rVsUv2yCqN5v9e61T29VDHSKa`,
         }).done(function (data) {
+
+            //Empties the article container so repeat selects don't spawn more than 12 articles 
             $('.generated-articles-container').empty();
+
+            //Filters results for whether or not articles contain an image 
             const filteredResults = data.results.filter(function (result) {
                 return result.multimedia[4] !== undefined;
             });
+
+            //If the API is working but returns no suitable articles, this error message is displayed  
             if (filteredResults.length === 0) {
                 $('.generated-articles-container').append('<p>Sorry, nothing found! Please try again.</p>');
             } else {
+
+                //Takes twelve articles from the filtered results 
                 filteredResults.slice(0, 12).forEach(function (item) {
+
+                    //Appends the article container with a link, image and article caption  
                     $('.generated-articles-container').append('<a href="' + item.url + '">' + '<article class="generated-articles" style="background-image: url(' + item.multimedia[4].url + ');">' + '<p class="article-caption">' + item.abstract + '</p></article></a>');
-                    $("img").removeClass("large-logo").addClass("small-logo");
-                    $("main").removeClass("main-container-large").addClass("main-container-small");
-                    $("header").removeClass("header-container-large").addClass("header-container-small");
-                    $("nav").removeClass("navigation-container-large").addClass("navigation-container-small");
-                });
-            }
+
+                    // Changes header, nav and logo from large to small when results are returned 
+                    $("img").removeClass("large-logo").addClass("small-logo").wrap('<a href="https://sarahellisfox.github.io/instanews/"></a>');
+                $("main").removeClass("main-container-large").addClass("main-container-small");
+                $("header").removeClass("header-container-large").addClass("header-container-small");
+                $("nav").removeClass("navigation-container-large").addClass("navigation-container-small");
+
+            });
+    }
+
+            //If the above request fails entirely this error message is displayed. Empty is used to keep the error messages from spawning each time it is returned
         }).fail(function () {
-            $('.generated-articles-container').empty();
-            $('.generated-articles-container').append('<p>Sorry, nothing found! Please try again.</p>');
-        }).always(function () {
-            $('#loading-icon').hide();
-        });
+        $('.generated-articles-container').empty();
+        $('.generated-articles-container').append('<p>Sorry, nothing found! Please try again.</p>');
+
+        // Hides the loading icon after content is loaded
+    }).always(function () {
+        $('#loading-icon').hide();
+    });
     });
 });
-
-
